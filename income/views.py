62db28da8d576558e7c71c47c208d904f9d8dd7b
hamidnet220 <kiani.hamidreza@gmail.com>
2019-10-01 11:16:28 +0330

    add season_report to view module income project.

--- income/views.py
+++ income/views.py
@@ -12,8 +12,37 @@ class home_view(TemplateView):
 # Incomes view
 def get_incomes_view(request,*args,**kwargs):
     ordering=('year','month','day')
-    view=ViewGenerator(Income,{},False,'add_income',ordering)
+    view=ViewGenerator(table=Income,opration_buttons={},
+                        select_checkbox=False,add_url='add_income',ordering=ordering)
+
     return render(request,"list_objects.html",view.get_context_template())
+
+#Session Report view
+def get_season_view(request,*args,**kwargs):
+
+    if request.method=='POST':
+        if request.POST['season']=='1':
+            incoms=Income.objects.filter(month__in=[1,2,3]).filter(year=1398)
+        elif request.POST['season']=='2':
+            incoms=Income.objects.filter(month__in=[4,5,6]).filter(year=1398)
+        elif request.POST['season']=='3':
+            incoms=Income.objects.filter(month__in=[7,8,9]).filter(year=1398)
+        elif request.POST['season']=='4':
+            incoms=Income.objects.filter(month__in=[10,11,12]).filter(year=1398)
+            
+        ordering=('year','month','day')
+        form=SeasonReport(request.POST)
+        view=ViewGenerator(table=Income,entities=incoms,opration_buttons={},
+                    select_checkbox=False,add_url='add_income',ordering=ordering)
+        contxt=view.get_context_template()
+        additon={'form':form}
+        contxt.update(**additon)
+        return render(request,"season_report.html",contxt)
+    else:    
+        print("Hello there I am before post method.")
+        form=SeasonReport()
+
+    return render(request,"season_report.html",{'form':form})
 
 # Employeers view
 def get_employeers_view(request,*args,**kwargs):
