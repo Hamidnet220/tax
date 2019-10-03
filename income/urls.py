2ef21f91e7ea08cc1d8fbad680500919be7c6ee6
hamidnet220 <kiani.hamidreza@gmail.com>
2019-10-03 15:56:53 +0330

    add paymnet report and season report in income urls.py.

--- income/urls.py
+++ income/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from .views import *
 urlpatterns=[
-    path("season_report/",get_season_view,name="season_report")
+    path("season_report/",get_season_view,name="season_report"),
+    path("employer_payment/",get_employer_payment_view,name="employer_payment")
 ]
