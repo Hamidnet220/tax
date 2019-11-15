a3371e354cc5476e6363b86a5c66ffbec7285455
hamidnet220 <kiani.hamidreza@gmail.com>
2019-11-16 02:19:53 +0330

    add localip to allowed host tax/settings.

--- tax/settings.py
+++ tax/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f(own&zqhm!owcw1!e@85q19nc$=+9*$^gdko_p-a=-bj%-wil'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.53','localhost','127.0.0.1']
 
 
 # Application definition
